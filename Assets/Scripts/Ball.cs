9e9ec47bd7481db3a75400593eddff24e9ab8205
Sunave <antti.suniala@gmail.com>
2015-07-04 17:09:42 +0300

    Add logic to launch Ball from the Paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+
+	private bool hasStarted = false;
+
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		print(paddleToBallVector.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			if (Input.GetMouseButtonDown(0)) {
+				print ("Left mouse button pressed");
+				hasStarted = true;
+				this.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+}
