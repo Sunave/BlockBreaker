021077c8d5285dfd9d338f3255b5a37be91e0ed5
Sunave <antti.suniala@gmail.com>
2015-07-05 17:13:11 +0300

    Change Paddle to polygon, refactor Paddle script

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Paddle : MonoBehaviour {
 
 	public bool autoPlay = false;
+	public float minX, maxX;
 
 	private Ball ball;
 
@@ -12,24 +13,27 @@ public class Paddle : MonoBehaviour {
 	}
 	
 	void Update () {
-		if (!autoPlay) {
-			MoveWithMouse();
+		Move();
+	}
+
+	void Move () {
+		float posInBlocks;
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
+
+		if (autoPlay) { 
+			posInBlocks = ball.transform.position.x;
 		} else {
-			AutoPlay();
+			posInBlocks = Input.mousePosition.x / Screen.width * 16;
 		}
-	}
 
-	void MoveWithMouse () {
-		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
-		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		paddlePos.x = Mathf.Clamp(posInBlocks, minX, maxX);
 		this.transform.position = paddlePos;
 	}
 
-	void AutoPlay() {
-		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
-		float ballPos = ball.transform.position.x;
-		paddlePos.x = Mathf.Clamp(ballPos, 0.5f, 15.5f);
-		this.transform.position = paddlePos;
-	}
+//	void AutoPlay() {
+//		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
+//		float ballPos = ball.transform.position.x;
+//		paddlePos.x = Mathf.Clamp(ballPos, minX, maxX);
+//		this.transform.position = paddlePos;
+//	}
 }
